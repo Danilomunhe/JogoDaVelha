4dfd3973beb3235465b7ba7c8e9b9fc83f2a75b9
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-03-15 16:53:34 -0300

    aula dia 15/03 - fazendo o layout da tela principal e tentando descobrir como desabilitar um botão

--- app/src/main/java/com/senai/jogodavelha/MainActivity.kt
+++ app/src/main/java/com/senai/jogodavelha/MainActivity.kt
@@ -2,10 +2,28 @@ package com.senai.jogodavelha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val posicao1 = findViewById<Button>(R.id.posicao1)
+        val posicao2 = findViewById<Button>(R.id.posicao2)
+
+
+            posicao1.setOnClickListener(){
+                posicao1.background = getDrawable(R.drawable.icone)
+                //Descobrir como desabilitar o botão
+            }
+
+
+
+        posicao2.setOnClickListener(){
+            posicao2.background = getDrawable(R.drawable.circulo)
+        }
+
     }
 }
