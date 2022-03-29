0f4ad20d3848b1ae80109e89492e4ed94f70aa48
DANILO DA CRUZ MUNHE <21276321@CFP127ALUNO.LOCAL>
2022-03-29 16:56:37 -0300

    aula dia 29/03 - player vs player finalizado

--- app/src/main/java/com/senai/jogodavelha/MainActivity.kt
+++ app/src/main/java/com/senai/jogodavelha/MainActivity.kt
@@ -24,137 +24,214 @@ class MainActivity : AppCompatActivity() {
         val posicao7 = findViewById<Button>(R.id.posicao7)
         val posicao8 = findViewById<Button>(R.id.posicao8)
         val posicao9 = findViewById<Button>(R.id.posicao9)
-        var teste = findViewById<TextView>(R.id.teste)
-        var valorJogadaO = ""
         var contadorJogadas: Int = 0
-        var valorJogadaX = ""
+        val arrayJogadasX = IntArray(9)
+        val arrayJogadasO = IntArray(9)
 
 
         posicao1.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao1.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao1.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[0] = 1
             } else if (contadorJogadas % 2 != 0) {
-                posicao1.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao1.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[0] = 1
             }
             posicao1.setEnabled(false)
             contadorJogadas++
-        }
 
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
+        }
         posicao2.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao2.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao2.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[1] = 2
             } else if (contadorJogadas % 2 != 0) {
-                posicao2.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao2.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[1] = 2
             }
             posicao2.setEnabled(false)
             contadorJogadas++
+
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
         }
 
         posicao3.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao3.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao3.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[2] = 3
             } else if (contadorJogadas % 2 != 0) {
-                posicao3.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao3.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[2] = 3
 
             }
             posicao3.setEnabled(false)
-            teste.text = valorJogadaX
             contadorJogadas++
 
-            valorJogadaX.toSortedSet()
-            if(valorJogadaX =="024"){
-                Toast.makeText(this, "Você ganhou!", Toast.LENGTH_SHORT).show()
-            }
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
         }
 
         posicao4.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao4.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao4.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[3] = 4
             } else if (contadorJogadas % 2 != 0) {
-                posicao4.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao4.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[3] = 4
 
             }
             posicao4.setEnabled(false)
-            teste.text = valorJogadaX
             contadorJogadas++
+
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
 
         }
 
         posicao5.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao5.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao5.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[4] = 5
             } else if (contadorJogadas % 2 != 0) {
-                posicao5.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao5.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[4] = 5
 
             }
             posicao5.setEnabled(false)
-            teste.text = valorJogadaX
             contadorJogadas++
+
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
         }
 
         posicao6.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao6.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao6.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[5] = 6
             } else if (contadorJogadas % 2 != 0) {
-                posicao6.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao6.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[5] = 6
             }
             posicao6.setEnabled(false)
-            teste.text = valorJogadaX
             contadorJogadas++
+
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
         }
 
         posicao7.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao7.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao7.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[6] = 7
             } else if (contadorJogadas % 2 != 0) {
-                posicao7.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao7.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[6] = 7
             }
             posicao7.setEnabled(false)
-            teste.text = valorJogadaX
             contadorJogadas++
+
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
         }
 
         posicao8.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao8.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao8.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[7] = 8
             } else if (contadorJogadas % 2 != 0) {
-                posicao8.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao8.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[7] = 8
             }
             posicao8.setEnabled(false)
-            teste.text = valorJogadaX
             contadorJogadas++
+
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
         }
 
         posicao9.setOnClickListener() {
             if (contadorJogadas % 2 == 0) {
-                posicao9.background = getDrawable(R.drawable.circulo)
-                valorJogadaX += contadorJogadas.toString()
+                posicao9.background = getDrawable(R.drawable.marca)
+                arrayJogadasX[8] = 9
             } else if (contadorJogadas % 2 != 0) {
-                posicao9.background = getDrawable(R.drawable.icone)
-                valorJogadaO += contadorJogadas.toString()
+                posicao9.background = getDrawable(R.drawable.circulo)
+                arrayJogadasO[8] = 9
             }
             posicao9.setEnabled(false)
-            teste.text = valorJogadaX
             contadorJogadas++
+
+            resultadoX(arrayJogadasX)
+            resultadoO(arrayJogadasO)
+
         }
 
 
-   teste.text = valorJogadaX
     }
+
+    private fun desativarBotoes() {
+        posicao1.setEnabled(false)
+        posicao2.setEnabled(false)
+        posicao3.setEnabled(false)
+        posicao4.setEnabled(false)
+        posicao5.setEnabled(false)
+        posicao6.setEnabled(false)
+        posicao7.setEnabled(false)
+        posicao8.setEnabled(false)
+        posicao9.setEnabled(false)
+    }
+
+    private fun resultadoX(arrayJogadasX: IntArray) {
+        if (arrayJogadasX[0] == 1 && arrayJogadasX[1] == 2 && arrayJogadasX[2] == 3) {
+            desativarBotoes()
+            return Toast.makeText(this, "X ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasX[3] == 4 && arrayJogadasX[4] == 5 && arrayJogadasX[5] == 6) {
+            desativarBotoes()
+            return Toast.makeText(this, "X ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasX[6] == 7 && arrayJogadasX[7] == 8 && arrayJogadasX[8] == 9) {
+            desativarBotoes()
+            return Toast.makeText(this, "X ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasX[2] == 3 && arrayJogadasX[5] == 6 && arrayJogadasX[8] == 9) {
+            desativarBotoes()
+            return Toast.makeText(this, "X ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasX[1] == 2 && arrayJogadasX[4] == 5 && arrayJogadasX[7] == 8) {
+            desativarBotoes()
+            return Toast.makeText(this, "X ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasX[6] == 7 && arrayJogadasX[4] == 5 && arrayJogadasX[2] == 3) {
+            desativarBotoes()
+            return Toast.makeText(this, "X ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasX[0] == 1 && arrayJogadasX[4] == 5 && arrayJogadasX[8] == 9) {
+            desativarBotoes()
+            return Toast.makeText(this, "X ganhou!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun resultadoO(arrayJogadasO: IntArray) {
+        if (arrayJogadasO[0] == 1 && arrayJogadasO[1] == 2 && arrayJogadasO[2] == 3) {
+            desativarBotoes()
+            return Toast.makeText(this, "bolinha ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasO[3] == 4 && arrayJogadasO[4] == 5 && arrayJogadasO[5] == 6) {
+            desativarBotoes()
+            return Toast.makeText(this, "bolinha ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasO[6] == 7 && arrayJogadasO[7] == 8 && arrayJogadasO[8] == 9) {
+            desativarBotoes()
+            return Toast.makeText(this, "bolinha ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasO[2] == 3 && arrayJogadasO[5] == 6 && arrayJogadasO[8] == 9) {
+            desativarBotoes()
+            return Toast.makeText(this, "bolinha ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasO[1] == 2 && arrayJogadasO[4] == 5 && arrayJogadasO[7] == 8) {
+            desativarBotoes()
+            return Toast.makeText(this, "bolinha ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasO[6] == 7 && arrayJogadasO[4] == 5 && arrayJogadasO[2] == 3) {
+            desativarBotoes()
+            return Toast.makeText(this, "bolinha ganhou!", Toast.LENGTH_LONG).show()
+        } else if (arrayJogadasO[0] == 1 && arrayJogadasO[4] == 5 && arrayJogadasO[8] == 9) {
+            desativarBotoes()
+            return Toast.makeText(this, "bolinha ganhou!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
 }
